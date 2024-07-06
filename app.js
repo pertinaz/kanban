6621279c2aca5b0cd29a585c2c05c5896ed30161
pertinaz <manuel.agudelo1@utp.edu.co>
2024-07-06 10:11:14 -0500

    pre-finishing the backend config settings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 
+
 const app = express(); // initializes express instance
 
 app.use(express.json()); // enables json files parsing
---
+++ src/middlewares/auth.js
@@ -0,0 +1,41 @@
+// import a user-creation model
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    // verify if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser)
+      return res.status(400).json({ message: "Username already exists" });
+
+    // verify if the email already exists
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail)
+      return res.status(400).json({ message: "Email already exists" });
+
+    // validate the password
+
+    // password should be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character
+
+    // has the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create the new user
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+
+    // create JWT token
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "2d",
+    }); // modify creating a component with tokenCreations and then import it here.
+
+    // send the user object and the JWT token in the response
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
